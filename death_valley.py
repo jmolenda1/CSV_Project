33db73c5aa4466a743cc4d1844a7b3e3a877470c
John Molenda <John_Molenda1@baylor.edu>
2021-02-22 14:14:29 -0600

    2/22 changes

---
+++ death_valley.py
@@ -0,0 +1,67 @@
+import csv
+from datetime import datetime
+open_file = open("death_valley_2018_simple.csv", "r")
+
+csv_file = csv.reader(open_file, delimiter=",")
+
+header_row = next(csv_file)
+print(type(header_row))
+
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
+
+
+
+highs = []
+dates = []
+lows = []
+
+#this is an example
+
+mydate = '2018-07-01'
+converted_date = datetime.strptime(mydate,'%Y-%m-%d')
+
+print(converted_date)
+
+
+
+for row in csv_file:
+    try:
+        high.append(int(row[4]))
+        low.append(int(row[5]))
+        converted_date = datetime.strptime(row[2],'%Y-%m-%d')
+    except ValueError:
+        print(f"missing data for {converted_date}")
+    else:
+        highs.append(high)
+        lows.append(low)
+        dates.append(converted_date)
+
+
+
+#print(highs)
+
+import matplotlib.pyplot as plt
+
+fig=plt.figure()
+
+plt.plot(dates, highs, c="red")
+plt.plot(dates, lows, c ="blue")
+
+fig.autofmt_xdate()
+
+plt.fill_between(dates,highs,lows,facecolor='blue',alpha=0.1)
+
+
+plt.title("Daily high temperatures, July 2018", fontsize=16)
+plt.xlabel("", fontsize=12)
+plt.ylabel("Temperature (F)", fontsize=12)
+plt.tick_params(axis="both", which="major", labelsize=12)
+
+plt.show()
+
+fig2, a = plt.subplots(2)
+a[0].plt(dates,highs,c='red')
+a[1].plot(dates,lows,c='blue')
+
+plt.show()
